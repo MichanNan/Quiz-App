942a6e9d60c00cbad7b08dfdf5a2a33d59ab7d52
Nan Cao <caonanla@gmail.com>
2023-05-05 12:51:43 +0200

    add toggle show answer button

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ bookmarks.forEach((element) => {
     element.classList.toggle("filled");
   });
 });
+
+const showAnsweButtons = document.querySelectorAll(
+  ".question-card__question__show-answer"
+);
+const answers = document.querySelectorAll(".question-card__answer");
+
+for (let i = 0; i < showAnsweButtons.length; i++) {
+  showAnsweButtons[i].addEventListener("click", () => {
+    answers[i].classList.toggle("hidden");
+    answers[i].classList.contains("hidden")
+      ? (showAnsweButtons[i].textContent = "Show Answer")
+      : (showAnsweButtons[i].textContent = "Hide Answer");
+  });
+}
