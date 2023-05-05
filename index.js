8f82f6856518e5cc9d8e54ab027d2820e4307796
Nan Cao <caonanla@gmail.com>
2023-05-05 18:46:17 +0200

    add new card function and add check the characters left function

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
+const showAnsweButtons = document.querySelectorAll(
+  ".question-card__question__show-answer"
+);
+const answers = document.querySelectorAll(".question-card__answer");
+const questionCard = document.querySelectorAll(".question-card");
+
 bookmarks.forEach((element) => {
   element.addEventListener("click", () => {
     element.classList.toggle("filled");
   });
 });
 
-const showAnsweButtons = document.querySelectorAll(
-  ".question-card__question__show-answer"
-);
-const answers = document.querySelectorAll(".question-card__answer");
-
 for (let i = 0; i < showAnsweButtons.length; i++) {
   showAnsweButtons[i].addEventListener("click", () => {
     answers[i].classList.toggle("hidden");
     answers[i].classList.contains("hidden")
-      ? (showAnsweButtons[i].textContent = "Show Answer")
-      : (showAnsweButtons[i].textContent = "Hide Answer");
+      ? ((showAnsweButtons[i].textContent = "Show Answer"),
+        (questionCard[i].style.height = "240px"))
+      : ((showAnsweButtons[i].textContent = "Hide Answer"),
+        (questionCard[i].style.height = "280px"));
   });
 }
